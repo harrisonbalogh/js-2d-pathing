e3d66ad9f7bd4c8adcec5f10bbd6b0668a8d7dfb
Harrison Balogh <harrisonbalogh@gmail.com>
2022-07-31 15:20:24 -0700

    Test new deploy flow

---
+++ deploy/deploy_test.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+echo $HTTP_DIR
+
+# if [ $# -ne 1 ] || [ ! -d "$1" ]
+# then
+#   exit 1
+# fi
+
+# path=/var/www/staging/projects
+# mkdir -p $path
+# rsync -a --exclude='.*' $1 $path
